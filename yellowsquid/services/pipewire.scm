11a2e33b48d1b6f120b6c189d0ed0ff190b33ebd
Chloe Brown <chloe.brown.00@outlook.com>
2021-12-29 17:39:31 +0000

    services: separate rtkit from pipewire

--- yellowsquid/services/pipewire.scm
+++ yellowsquid/services/pipewire.scm
@@ -3,33 +3,9 @@
   #:use-module (gnu home services shepherd)
   #:use-module (gnu packages linux)
   #:use-module (gnu services)
-  #:use-module (gnu services dbus)
-  #:use-module (gnu system shadow)
   #:use-module (guix gexp)
-  #:use-module (yellowsquid packages rtkit)
-  #:export (rtkit-service-type
-            home-pipewire-service-type
+  #:export (home-pipewire-service-type
             home-wireplumber-service-type))
-
-(define %rtkit-accounts
-  (list (user-group (name "rtkit") (system? #t))
-        (user-account
-         (name "rtkit")
-         (group "rtkit")
-         (system? #t)
-         (comment "RealtimeKit daemon user")
-         (home-directory "/var/empty")
-         (shell "/run/current-system/profile/sbin/nologin"))))
-
-(define rtkit-service-type
-  (service-type (name 'rtkit)
-                (extensions
-                 (list (service-extension dbus-root-service-type
-                                          list)
-                       (service-extension account-service-type
-                                          (const %rtkit-accounts))))
-                (default-value rtkit)
-                (description "Realtime Kit scheduling policy daemon.")))
 
 ;; TODO: add dependency on rtkit-service-type.
 (define (home-pipewire-shepherd-service pipewire)
