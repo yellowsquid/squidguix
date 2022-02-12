85b49957b3ef958bbeba7edd7e5e2fa733ba2a8b
Chloe Brown <chloe.brown.00@outlook.com>
2022-02-12 16:26:57 +0000

    services: mako: new service

---
+++ yellowsquid/services/mako.scm
@@ -0,0 +1,36 @@
+(define-module (yellowsquid services mako)
+  #:use-module (gnu home services)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu services configuration)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:export (mako-configuration
+            mako-service-type))
+
+(define-configuration mako-configuration
+  (package
+   (package mako)
+   "Package to use")
+  (config-parts
+   (text-config '())
+   "Configuration file parts"))
+
+(define (mako-files config)
+  `(("config/mako/config"
+     ,(serialize-configuration
+       config
+       (filter-configuration-fields
+        mako-configuration-fields
+        'config-parts)))))
+
+(define mako-service-type
+  (service-type (name 'mako)
+                (extensions
+                 (list (service-extension
+                        home-files-service-type
+                        mako-files)
+                       (service-extension
+                        home-profile-service-type
+                        mako-configuration-package)))
+                (default-value (mako-configuration))
+                (description "Install and configure mako.")))
