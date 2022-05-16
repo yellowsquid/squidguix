730c106ccc87fedb657ad6c18e80b2431d51d5ff
Chloe Brown <chloe.brown.00@outlook.com>
2022-05-16 16:04:03 +0100

    Bump linux version.

--- yellowsquid/packages/linux.scm
+++ yellowsquid/packages/linux.scm
@@ -2,7 +2,7 @@
   #:use-module (guix packages)
   #:use-module (nongnu packages linux))
 
-(define %latest-version "5.17.7")
+(define %latest-version "5.17.8")
 
 (define-public linux-latest
   (package
@@ -11,4 +11,4 @@
     (version %latest-version)
     (source ((@@ (gnu packages linux) %upstream-linux-source)
              %latest-version
-             (base32 "16ccf7n6fns9z93c65lchn5v3fgl9c5vkr1v6p0c1xifn7v7xxi2")))))
+             (base32 "0si34i1hrhbapv1a8xghyvkywij0wgidxhdan23d6y2iqzc62y8i")))))
