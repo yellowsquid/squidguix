5d4274c550e8da934350137b4a6d0ac0538d326f
Chloe Brown <chloe.brown.00@outlook.com>
2022-05-29 14:37:02 +0100

    Bump Linux version to 5.18.

--- yellowsquid/packages/linux.scm
+++ yellowsquid/packages/linux.scm
@@ -2,7 +2,7 @@
   #:use-module (guix packages)
   #:use-module (nongnu packages linux))
 
-(define %latest-version "5.17.9")
+(define %latest-version "5.18")
 
 (define-public linux-latest
   (package
@@ -11,4 +11,4 @@
     (version %latest-version)
     (source ((@@ (gnu packages linux) %upstream-linux-source)
              %latest-version
-             (base32 "0y2rmn86z3cvgv71b6sjjyafnlbanlib1kjpjjqzjbgg86y2890p")))))
+             (base32 "1vjwhl4s8qxfg1aabn8xnpjza3qzrjcp5450h9qpjvl999lg3wsi")))))
