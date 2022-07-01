ed35cb78b575291966c93693d4dc1848e5299c41
Chloe Brown <chloe.brown.00@outlook.com>
2022-07-01 13:59:58 +0100

    Add native compilation to emacs.
    Credit to Andrew Whatson for doing all the work.
    
    (https://github.com/flatwhatson/guix-channel)

---
+++ yellowsquid/packages/emacs.scm
@@ -0,0 +1,94 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2020 Andrew Whatson <whatson@gmail.com>
+;;;
+;;; This file is NOT part of GNU Guix.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (yellowsquid packages emacs)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages xorg)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (ice-9 regex)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-26)
+  #:use-module (yellowsquid packages)
+  #:use-module (yellowsquid packages gcc))
+
+(define emacs-with-native-comp
+  (lambda* (emacs gcc #:optional full-aot)
+    (let ((libgccjit (libgccjit-for-gcc gcc)))
+      (package
+        (inherit emacs)
+        (source
+         (origin
+           (inherit (package-source emacs))
+           (patches
+            (append (search-patches "emacs-native-comp-exec-path.patch")
+                    (filter
+                     (lambda (f)
+                       (not (any (cut string-match <> f)
+                                 '("/emacs-exec-path\\.patch$"
+                                   "/emacs-ignore-empty-xim-styles\\.patch$"))))
+                     (origin-patches (package-source emacs)))))))
+        (arguments
+         (substitute-keyword-arguments (package-arguments emacs)
+           ((#:make-flags flags ''())
+            (if full-aot
+                #~(cons* "NATIVE_FULL_AOT=1" #$flags)
+                flags))
+           ((#:configure-flags flags)
+            #~(cons* "--with-native-compilation" #$flags))
+           ((#:phases phases)
+            #~(modify-phases #$phases
+               ;; Add build-time library paths for libgccjit.
+               (add-before 'configure 'set-libgccjit-path
+                 (lambda* (#:key inputs #:allow-other-keys)
+                   (let ((libgccjit-libdir
+                          (string-append (assoc-ref inputs "libgccjit")
+                                         "/lib/gcc/" %host-type "/"
+                                         #$(package-version libgccjit) "/")))
+                     (setenv "LIBRARY_PATH"
+                             (string-append libgccjit-libdir ":"
+                                            (getenv "LIBRARY_PATH"))))
+                   #t))
+               ;; Add runtime library paths for libgccjit.
+               (add-after 'unpack 'patch-driver-options
+                 (lambda* (#:key inputs #:allow-other-keys)
+                   (substitute* "lisp/emacs-lisp/comp.el"
+                     (("\\(defcustom native-comp-driver-options nil")
+                      (format
+                       #f "(defcustom native-comp-driver-options '(~s ~s ~s ~s)"
+                       (string-append
+                        "-B" (assoc-ref inputs "binutils") "/bin/")
+                       (string-append
+                        "-B" (assoc-ref inputs "glibc") "/lib/")
+                       (string-append
+                        "-B" (assoc-ref inputs "libgccjit") "/lib/")
+                       (string-append
+                        "-B" (assoc-ref inputs "libgccjit") "/lib/gcc/"))))
+                   #t))))))
+        (native-inputs
+         (modify-inputs (package-native-inputs emacs)
+           (prepend gcc)))
+        (inputs
+         (modify-inputs (package-inputs emacs)
+           (prepend glibc libgccjit libxcomposite)))))))
+
+(define-public emacs-native-comp
+  (emacs-with-native-comp emacs gcc-12))
