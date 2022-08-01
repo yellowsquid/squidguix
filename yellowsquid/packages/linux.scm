3ad828eb5800bf33fdbae0861c434e3bb504614b
Chloe Brown <chloe.brown.00@outlook.com>
2022-08-01 11:43:21 +0100

    linux-latest: upgrade to 5.19.

--- yellowsquid/packages/linux.scm
+++ yellowsquid/packages/linux.scm
@@ -2,7 +2,7 @@
   #:use-module (guix packages)
   #:use-module (nongnu packages linux))
 
-(define %latest-version "5.18.15")
+(define %latest-version "5.19")
 
 (define-public linux-latest
   (package
@@ -11,4 +11,4 @@
     (version %latest-version)
     (source ((@@ (gnu packages linux) %upstream-linux-source)
              %latest-version
-             (base32 "0g5yvhq7rmkzvfl4w50l7bg56a20insvg4s4nvgnk2iqvkmlz039")))))
+             (base32 "1a05a3hw4w3k530mxhns96xw7hag743xw5w967yazqcykdbhq97z")))))
