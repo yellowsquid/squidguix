7976124ffcee6310c7cca2fc0ba2d3ba354d45bb
Chloe Brown <chloe.brown.00@outlook.com>
2023-05-11 14:57:52 +0100

    swaylock-effects: bump version.

--- yellowsquid/packages/swaylock.scm
+++ yellowsquid/packages/swaylock.scm
@@ -1,6 +1,7 @@
 (define-module (yellowsquid packages swaylock)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages wm)
+  #:use-module (guix git-download)
   #:use-module (guix packages))
 
 (define-public swaylock-pam
@@ -12,6 +13,17 @@
 (define-public swaylock-effects-pam
   (package
     (inherit swaylock-effects)
+    (version "1.6.11")
     (name "swaylock-effects")
-    (inputs (modify-inputs (package-inputs swaylock)
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/jirutka/swaylock-effects")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "0j7dxn66xqlf6iv2arqzz7mxlh7nf85anvpyf30d2frcidarda9h"))))
+    (inputs (modify-inputs (package-inputs swaylock-effects)
                (append linux-pam)))))
