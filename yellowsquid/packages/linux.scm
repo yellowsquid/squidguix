6a4eb0e417411fb83776c6c9bfa6b968756172cd
Chloe Brown <chloe.brown.00@outlook.com>
2024-08-23 13:31:07 +0100

    [chore] fix lint errors in linux.

--- yellowsquid/packages/linux.scm
+++ yellowsquid/packages/linux.scm
@@ -11,7 +11,8 @@
   #:use-module (guix download)
   #:use-module (guix gexp)
   #:use-module (guix git-download)
-  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module ((guix licenses)
+                #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (nongnu packages linux)
@@ -21,30 +22,30 @@
   (package
     (name "linuxconsole-tools")
     (version "1.8.1")
-    (source (origin (method url-fetch)
-                    (uri (string-append
-                          "mirror://sourceforge/linuxconsole/linuxconsoletools-"
-                          version ".tar.bz2"))
-                    (patches
-                     (search-patches "linuxconsole-udev-install.patch"))
-                    (sha256
-                     (base32
-                      "0mi4bkrwp83gcg3jy7knscfi4qziggkljz7kylcdpdw2qx2rg8jd"))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "mirror://sourceforge/linuxconsole/linuxconsoletools-" version
+             ".tar.bz2"))
+       (patches (search-patches "linuxconsole-tools-udev-install.patch"))
+       (sha256
+        (base32 "0mi4bkrwp83gcg3jy7knscfi4qziggkljz7kylcdpdw2qx2rg8jd"))))
     (inputs (list sdl2))
     (native-inputs (list pkg-config))
     (build-system gnu-build-system)
     (arguments
-     `(#:make-flags
-       (list (string-append "CC=" ,(cc-for-target))
-             "DESTDIR="
-             (string-append "PREFIX=" %output))
-       #:phases
-       (modify-phases %standard-phases
-         (delete 'configure)
-         (delete 'check))))
-    (home-page "https://sourceforge.net/projects/linuxconsole")
-    (synopsis "Utilities to test and configure joysticks and the event input layer.")
-    (description "Linux Console tools includes utilities to test and configure
+     `(#:make-flags (list (string-append "CC="
+                                         ,(cc-for-target)) "DESTDIR="
+                                         (string-append "PREFIX=" %output))
+       #:phases (modify-phases %standard-phases
+                  (delete 'configure)
+                  (delete 'check))))
+    (home-page "https://sourceforge.net/projects/linuxconsole/")
+    (synopsis
+     "Utilities to test and configure joysticks and the event input layer")
+    (description
+     "Linux Console tools includes utilities to test and configure
 joysticks, connect legacy devices to the kernel's input subsystem (providing
 support for serial mice, touchscreens etc.), and test the input event layer.
 
@@ -70,27 +71,27 @@ support for serial mice, touchscreens etc.), and test the input event layer.
     (source
      (origin
        (method git-fetch)
-       (uri (git-reference (url home-page)
-                           (commit (string-append "v" version))))
+       (uri (git-reference
+             (url home-page)
+             (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (patches (search-patches "wihotspot-destdir.patch"))
-       (sha256 (base32 "0fd22rppbj3kbgx1qkiaj8g3kagc0941q2xd59z0pj041rcxhqgr"))))
+       (sha256
+        (base32 "0fd22rppbj3kbgx1qkiaj8g3kagc0941q2xd59z0pj041rcxhqgr"))))
     (build-system glib-or-gtk-build-system)
     (arguments
      (list
-      #:make-flags
-      #~(list (string-append "PREFIX=" #$output)
-              "PKGCONFIG=pkg-config")
-      #:phases
-      #~(modify-phases %standard-phases
-          (delete 'configure)
-          (delete 'check))))
+      #:make-flags #~(list (string-append "PREFIX="
+                                          #$output) "PKGCONFIG=pkg-config")
+      #:phases #~(modify-phases %standard-phases
+                   (delete 'configure)
+                   (delete 'check))))
     (inputs (list gtk+ qrencode))
     (propagated-inputs (list dnsmasq hostapd))
     (native-inputs (list pkg-config))
-    (synopsis "Wifi hotspot creator for Linux.")
+    (synopsis "Wifi hotspot creator for Linux")
     (description
      "Feature-rich wifi hotspot creator for Linux which provides both GUI and
-command-line interface. It is also able to create a hotspot using the same wifi
+command-line interface.  It is also able to create a hotspot using the same wifi
 card which is connected to an AP already.")
     (license license:bsd-2)))
